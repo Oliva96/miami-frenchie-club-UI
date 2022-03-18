2f39ecfeb9853dbb13876d4e9786fe6284d764f5
Oliva96 <oliva961206@gmail.com>
2022-03-18 13:28:41 -0500

    changes

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -9,52 +9,55 @@ import { Link, useLocation } from 'react-router-dom';
 
 const Footer = () => {
 
-  let location = useLocation();
+    let location = useLocation();
 
-  return (
-    <>
-    <div className='footer'>
-        <div>
-            <img src={logo} alt='footer-logo' className='footer-logo'/>
-            <div >
-                <a href='https://twitter.com/miafrenchieclub?s=21' target='_blank'  rel='noreferrer'>
-                    <BsTwitter className='footerItem'/>
-                </a>
-                <a href='https://discord.gg/ZtP8JFXeBF' target='_blank' rel='noreferrer'>
-                    <BsDiscord className='footerItem'/>
-                </a>
-                <a href='https://instagram.com/miamifrenchieclub?utm_medium=copy_link' target='_blank' rel='noreferrer'>
-                    <FaInstagramSquare className='footerItem'/>
-                </a>
-            </div>
-        </div>
-        <div style={{display: 'block', justifyContent: 'center', alignItems: 'center'}}>
-            {
-                location.pathname === '/buy' ? 
-                (
-                    <div className='footer-links-container'>
-                        <Link to='/' className='footerItem'>Home</Link>
+    return (
+        <>
+            <div className='footer'>
+                <div>
+                    <img src={logo} alt='footer-logo' className='footer-logo' />
+                    <div >
+                        <a href='https://twitter.com/miafrenchieclub?s=21' target='_blank' rel='noreferrer'>
+                            <BsTwitter className='footerItem' />
+                        </a>
+                        <a href='https://discord.gg/ZtP8JFXeBF' target='_blank' rel='noreferrer'>
+                            <BsDiscord className='footerItem' />
+                        </a>
+                        <a href='https://instagram.com/miamifrenchieclub?utm_medium=copy_link' target='_blank' rel='noreferrer'>
+                            <FaInstagramSquare className='footerItem' />
+                        </a>
                     </div>
-                ) : (
+                </div>
+                <div style={{ display: 'block', justifyContent: 'center', alignItems: 'center' }}>
+                    {
+                        location.pathname === '/buy' ?
+                            (
+                                <div className='footer-links-container'>
+                                    <Link to='/' className='footerItem'>Home</Link>
+                                </div>
+                            ) : (
+                                <div className='footer-links-container'>
+                                    {/* <Link to='/buy' className='footerItem'>Buy</Link> */}
+                                    <ScrollLink smooth to='about' className='footerItem'>about</ScrollLink>
+                                    <ScrollLink smooth to='roadmap' className='footerItem'>roadmap</ScrollLink>
+                                    <ScrollLink smooth to='team' className='footerItem'>team</ScrollLink>
+                                </div>
+                            )
+                    }
+
                     <div className='footer-links-container'>
-                        {/* <Link to='/buy' className='footerItem'>Buy</Link> */}
-                        <ScrollLink smooth to='about' className='footerItem'>about</ScrollLink>
-                        <ScrollLink smooth to='roadmap' className='footerItem'>roadmap</ScrollLink>
-                        <ScrollLink smooth to='team' className='footerItem'>team</ScrollLink>
+                        <a href='https://docs.google.com/document/d/e/2PACX-1vQcSyC6K82o0TYxMF-20gt4u6HmEdpqRYES2DNR49bMbu9S-K5drjnjekVQlL4i_W_yEIbq91FZPrjk/pub' target='_blank' rel='noreferrer'
+                            className='footerItem'>
+                            terms & conditions
+                        </a>
                     </div>
-                )
-            }
-            
-            <div className='footer-links-container'>
-                <Link to='/' className='footerItem'> terms & conditions </Link>
+                </div>
+                <div className='foot'>
+                    Cafecito labs llc.
+                </div>
             </div>
-        </div>
-        <div className='foot'>
-            All rights reserved by Cafecito labs llc.
-        </div>
-    </div>
-    </>
-  )
+        </>
+    )
 }
 
 export default Footer
